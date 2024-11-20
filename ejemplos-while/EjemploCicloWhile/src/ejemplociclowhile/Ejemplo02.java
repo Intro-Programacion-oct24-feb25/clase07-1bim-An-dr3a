94376ff86090508e3b3afb605341906e5c4237fa
Andrea Criollo <criolloandrea20@hotmail.com>
2024-11-20 11:54:45 -0500

    Comentarios y ejemplos agregados

--- ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo02.java
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo02.java
@@ -24,7 +24,8 @@ public class Ejemplo02 {
         limite = entrada.nextInt(); 
         while (contador <= limite){
             
-            if (contador%2!=0) {
+            if (contador%2 == 0) {
+                // != significa que no es igual, en este caso a cero
                 System.out.printf("Contador %d\n", 
                     contador);
             }
